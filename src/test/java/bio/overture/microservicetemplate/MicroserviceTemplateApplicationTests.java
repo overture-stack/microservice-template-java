a4292115762b65a433c1391515e14fbaa3658254
Jon Eubank <joneubank@gmail.com>
2017-12-07 19:38:00 -0500

    Simple formatting fixes

--- src/test/java/bio/overture/microservicetemplate/MicroserviceTemplateApplicationTests.java
+++ src/test/java/bio/overture/microservicetemplate/MicroserviceTemplateApplicationTests.java
@@ -25,8 +25,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MicroserviceTemplateApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }
