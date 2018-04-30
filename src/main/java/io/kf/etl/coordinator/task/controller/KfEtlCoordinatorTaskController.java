89754cdef19c89dd64ff35d5b53f024c7113da7a
Jon Eubank <joneubank@gmail.com>
2018-04-30 13:32:42 -0400

    Remove FSM bean, remove test controller

--- src/main/java/io/kf/etl/coordinator/task/controller/KfEtlCoordinatorTaskController.java
+++ src/main/java/io/kf/etl/coordinator/task/controller/KfEtlCoordinatorTaskController.java
@@ -3,11 +3,7 @@ package io.kf.etl.coordinator.task.controller;
 import io.kf.etl.coordinator.task.fsm.entity.StatusResponse;
 import io.kf.etl.coordinator.task.fsm.entity.TasksRequest;
 import io.kf.etl.coordinator.task.fsm.entity.TasksResponse;
-import io.kf.etl.coordinator.task.fsm.events.KfEtlCoordinatorTaskEvents;
-import io.kf.etl.coordinator.task.fsm.states.KfEtlCoordinatorTaskStates;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.statemachine.StateMachine;
 import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.Consumes;
@@ -15,9 +11,6 @@ import javax.ws.rs.Produces;
 
 @RestController
 public class KfEtlCoordinatorTaskController {
-
-    @Autowired
-    private StateMachine<KfEtlCoordinatorTaskStates, KfEtlCoordinatorTaskEvents> fsm;
 
     @GetMapping(path = "/status/")
     @Produces("application/json")
